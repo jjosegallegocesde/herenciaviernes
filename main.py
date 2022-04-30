deee7f61347d0072b74065f53772e631cfd5e1eb
Cristian Zuluaga <cczuluaga19@gmail.com>
2022-04-29 20:42:15 -0500

    herencia HU002

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#Herencia en python
+from clases.Cliente import Cliente#Herencia en python
+
+#crear un objeto de la clase hija 
+hijo=Cliente()
+
+hijo.nombre="Cristian C Zuluaga"
+hijo.saldo=5000
+hijo.clasificar
