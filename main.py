ccbd5c460c9432eb8aee94a8b240e1d5b8979fb5
Darvisechavarria <yerikechavarria210@gmail.com>
2022-04-29 19:24:24 -0500

    HU002 se crea una Herencia

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #Herencia en python
+from classes.Persona import Persona
+
+class Carro(Persona):
+    def __init__(self):
+        self.__placa=None
+
+    #Getters
+    @property
+    def placa(self):
+        return self.__placa
+    
+    #Setters
+    @placa.setter
+    def placa(self,placa):
+        self.__placa=placa
