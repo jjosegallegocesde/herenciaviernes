d5da48413325039f674247f8554a3e6a0420edc7
Natalia Morales <nataliamgal95@gmail.com>
2022-04-29 20:41:49 -0500

    HU002 Herencia

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #Herencia en python
+from clases.Cliente import Cliente
+
+#Crear un OBJETO de la clase hija:
+hijo=Cliente()
+
+#Acceder a los atributos
+hijo.nombre="Natalia Morales"
+hijo.saldo=5000
+hijo.clasificar()
