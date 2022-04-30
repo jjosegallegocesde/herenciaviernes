aad0c6e7691aed9855cdf569c9a3118c052e5e5b
luis Villa <nachovil74@gmail.com>
2022-04-29 20:22:19 -0500

    se crea nueva clase en HU002

---
+++ clases/cliente.py
@@ -0,0 +1,18 @@
+from clases.Persona import Persona
+
+class Cliente(Persona):
+    def __init__(self):
+        self.__saldo=None
+
+    @property
+    def saldo(self):
+        return(self.__saldo)
+
+    @saldo.setter
+    def saldo(self,saldo):
+        self.__saldo=saldo
+
+    def mostrarSaldo(self):
+        print(self.saldo)            
+
+        
--- clases/persona.py
+++ clases/persona.py
@@ -1,59 +1,65 @@
+from mailbox import NoSuchMailboxError
+
+
+# nombre
+# edad
+# telefono
+# direccion
+# correo 
+# saludar (hola me llamo) getters, setters
+
+ #creamos el constructor de la clase
 class Persona:
+    def __init__(self):
+      self.__nombre=None
+      self.__edad=None
+      self.__telefono=None
+      self.__direccion=None
+      self.__correo=None
+#GETTERS
+    @property
+    def nombre(self):
+        return self.__nombre    
 
-	 #creamos el constructor de la clase
-	def __init__(self):
-		self.__nombre = None
-		self.__edad = None
-		self.__telefono = None
-		self.__direccion = None
-		self.__correo = None
+    @property
+    def edad(self):
+        return self.__edad
 
-	@property
-	def nombre(self):
-		return(self.__nombre)
-		
-	@property
-	def edad(self):
-		return(self.__edad)
-	
-	@property
-	def telefono(self):
-		return(self.__telefono)
-	
-	@property
-	def direccion(self):
-		return(self.__direccion)
-	
-	@property
-	def correo(self):
-		return(self.__correo)
-	
-	@nombre.setter
-	def nombre(self,nombre):
-		self.__nombre =nombre
+    @property
+    def telefono(self):
+        return self.__telefono
 
-	@nombre.setter
-	def edad(self,edad):
-		self.__edad =edad
+    @property
+    def direccion(self):
+        return self.__direccion
 
-	@nombre.setter
-	def telefono(self,telefono):
-		self.__telefono =telefono
 
-	@nombre.setter
-	def telefono(self,telefono):
-		self.__telefono =telefono
-	
-	@nombre.setter
-	def direccion(self,direccion):
-		self.__direccion =direccion
+    @property
+    def correo(self):
+        return self.__correo 
 
-	@nombre.setter
-	def correo(self,correo):
-		self.__correo =correo
-	
-	def clasificar(self,nombre):
-		print(f"Hola mi nombre es {nombre}")
+#SETTERS
+    @nombre.setter
+    def nombre(self,nombre):
+         self.__nombre=nombre  
 
-		
-	 
+    @edad.setter
+    def edad(self,edad):
+         self.__edad=edad 
+
+    @telefono.setter
+    def telefono(self,telefono):
+         self.__telefono=telefono 
+
+    @direccion.setter
+    def direccion(self,direccion):
+         self.__direccion=direccion
+
+    @correo.setter
+    def correo(self,correo):
+         self.__correo=correo
+
+
+    def saludar(self):
+        print(f"Hola yo soy {self.nombre}")
+
